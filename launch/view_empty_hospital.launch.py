5ef712be58968bc21c2ece46fd1b18823483f1ee
Tobi A <t.alscher@tiplu.de>
2021-11-04 17:19:17 +0100

    changed model base project

--- launch/view_empty_hospital.launch.py
+++ launch/view_empty_hospital.launch.py
@@ -7,31 +7,33 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
+
 def get_robot_spawn_args():
     ROBOT_MODEL = os.environ['ROBOT_MODEL']
     POSE_INIT_X = os.environ['POSE_INIT_X']
     POSE_INIT_Y = os.environ['POSE_INIT_Y']
     POSE_INIT_Z = os.environ['POSE_INIT_Z']
 
-    robot_sdf = os.path.join(get_package_share_directory('map_server'),
-                         'models',
-                         ROBOT_MODEL,
-                         ROBOT_MODEL + '.sdf')
+    robot_sdf = os.path.join(get_package_share_directory('tiplu_world'),
+                             'models',
+                             ROBOT_MODEL,
+                             ROBOT_MODEL + '.sdf')
     robot_xml = open(robot_sdf, 'r').read()
     robot_xml = robot_xml.replace('"', '\\"')
 
     initial_pose = '{position: ' +\
-                    '{x: ' + POSE_INIT_X + ', ' +\
-                     'y: ' + POSE_INIT_Y + ', ' +\
-                     'z: ' + POSE_INIT_Z + '}}'
+        '{x: ' + POSE_INIT_X + ', ' +\
+        'y: ' + POSE_INIT_Y + ', ' +\
+        'z: ' + POSE_INIT_Z + '}}'
 
     spawn_args = '{name: \"' + ROBOT_MODEL + '\",' +\
                  'xml: \"' + robot_xml + '\",' +\
                  'initial_pose: ' + initial_pose + '}'
     return spawn_args
 
+
 def generate_launch_description():
-    launch_file_dir = os.path.join(get_package_share_directory('map_server'), 'launch')
+    launch_file_dir = os.path.join(get_package_share_directory('tiplu_world'), 'launch')
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
     ld = launch.LaunchDescription([
